6e73c84b104e212f1fd8bdac0a1597165f06fe69
Bart Stassen <hello@bartstassen.com>
2024-09-22 20:42:10 +0200

    timeouts

--- parcdesecrins.js
+++ parcdesecrins.js
@@ -1002,10 +1002,10 @@ function fadeDiv(divId, count) {
   // Start fade-out after the fade-in animation is complete
   setTimeout(() => {
       fadeDiv.classList.remove('fade-in-out'); // Fade out
-  }, 1000); // Duration of fade-in animation
+  }, 2000); // Duration of fade-in animation
 
   // wait for 1 second before updating the counter
   setTimeout(() => {
     $("#totalresults").html("<b>" + count + "</b> results within map area");
-}, 500); // Duration of fade-in animation
+}, 100); // Duration of fade-in animation
 }
