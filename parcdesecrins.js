d02d4e31736354b00909d7975f1a0490fc04a2df
Bart Stassen <hello@bartstassen.com>
2024-09-22 19:48:16 +0200

    match

--- parcdesecrins.js
+++ parcdesecrins.js
@@ -1005,5 +1005,5 @@ function fadeDiv(divId) {
   // Hide completely after fading out
   setTimeout(() => {
       //fadeDiv.style.display = 'none'; // Hide it completely after fading out
-  }, 4000); // Match this with the total duration (1s fade-in + 1s fade-out)
+  }, 2000); // Match this with the total duration (1s fade-in + 1s fade-out)
 }
