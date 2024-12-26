93911adfa6f643defba19224bdeb4ccbf2714f29
Bart Stassen <hello@bartstassen.com>
2024-12-26 11:47:09 +0100

    edit

--- parcdesecrins.js
+++ parcdesecrins.js
@@ -907,9 +907,9 @@ function populateAutoSuggest(featuresArray) {
     // console.log(feature);
     const li = document.createElement("li");
     //li.textContent = feature.place_name + (properties?.categories?.length > 0):// + feature.properties.categories[0];
-    li.innerHTML = `${feature.place_name} (${
+    li.innerHTML = `${feature.place_name} ${
       feature.properties?.categories?.length > 0 ? `<span class="geoloctag">${feature.properties.categories[0]}</span>` : ""
-    }.`;
+    }`;
 
     li.setAttribute("data-center", feature.center);
 
