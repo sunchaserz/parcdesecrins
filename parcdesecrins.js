c2bce2fc273f87fb9d2f5e20e0ddd79724c805a6
Bart Stassen <hello@bartstassen.com>
2024-12-26 11:30:02 +0100

    featyre

--- parcdesecrins.js
+++ parcdesecrins.js
@@ -908,7 +908,7 @@ function populateAutoSuggest(featuresArray) {
     const li = document.createElement("li");
     //li.textContent = feature.place_name + (properties?.categories?.length > 0):// + feature.properties.categories[0];
     li.textContent = `${feature.place_name} (${
-      properties?.categories?.length > 0 ? `has ${properties.categories[0]} items` : "is empty or missing"
+      feature.properties?.categories?.length > 0 ? `has ${feature.properties.categories[0]} items` : "is empty or missing"
     }.`;
 
     console.log(message);
