634beca2ade1ceea8c4ae4163c32884c1fde6afd
Bart Stassen <hello@bartstassen.com>
2024-12-28 14:37:12 +0100

    consle

--- parcdesecrins.js
+++ parcdesecrins.js
@@ -621,7 +621,9 @@ document.querySelector(".list-toggle").addEventListener("click", function () {
  */
 async function initializeCardsComponent() {
   await waitForFrameworkJS();
+  console.log("Framework.js is loaded");
   await waitForElement("#cards");
+  console.log("Cards DOM element is available");
   $app.createComponent("cards", initialData).mount("#cards");
 }
 
