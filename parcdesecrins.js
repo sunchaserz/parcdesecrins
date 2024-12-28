32bfb9d3e0825f363c43770b578741ed5dc06386
Bart Stassen <hello@bartstassen.com>
2024-12-28 12:33:08 +0100

    feelit

--- parcdesecrins.js
+++ parcdesecrins.js
@@ -144,9 +144,9 @@ function handleSuccessfulDataFetch(data) {
 
 function updateResultsDisplay(data) {
   let result_text = data.length == 1 ? "result" : "results";
-  console.log("Data fetched successfully");
   let result_searchterm =
     document.getElementById("search").value.toLowerCase() == "" ? "" : ' for <b>"' + document.getElementById("search").value.toLowerCase() + '"</b>';
+  console.log("Data fetched successfully");
   $("#totalresults").html("<b>" + data.length + "</b> " + result_text + result_searchterm);
 }
 
