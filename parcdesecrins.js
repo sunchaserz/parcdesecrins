830ff16213d825bf3a234fd299bc47e2d89c9610
Bart Stassen <hello@bartstassen.com>
2024-12-28 12:32:43 +0100

    ingredienten

--- parcdesecrins.js
+++ parcdesecrins.js
@@ -143,8 +143,8 @@ function handleSuccessfulDataFetch(data) {
 }
 
 function updateResultsDisplay(data) {
-  console.log("Data fetched successfully");
   let result_text = data.length == 1 ? "result" : "results";
+  console.log("Data fetched successfully");
   let result_searchterm =
     document.getElementById("search").value.toLowerCase() == "" ? "" : ' for <b>"' + document.getElementById("search").value.toLowerCase() + '"</b>';
   $("#totalresults").html("<b>" + data.length + "</b> " + result_text + result_searchterm);
