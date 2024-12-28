37b93500d61555aa7924b6723940df00515d7b04
Bart Stassen <hello@bartstassen.com>
2024-12-28 12:05:07 +0100

    cardloaded

--- parcdesecrins.js
+++ parcdesecrins.js
@@ -593,6 +593,13 @@ function fadeDiv(divId, count) {
   }, 1000);
 }
 
+// abusing the x-show  (see webflow on the card) functionality from framework.js to inject an id into the card
+function cardLoaded(card) {
+  //console.log("card loaded" + card.id);
+
+  return "#card-" + card.id;
+}
+
 // Event Listeners
 $("#search").on("input", function () {
   $(this).val() ? $(this).addClass("has--value") : $(this).removeClass("has--value");
