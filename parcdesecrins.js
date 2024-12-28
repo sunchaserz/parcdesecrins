7f75c2a755c85be6461b8c81d571e2a669d385d7
Bart Stassen <hello@bartstassen.com>
2024-12-28 12:31:40 +0100

    nothing

--- parcdesecrins.js
+++ parcdesecrins.js
@@ -129,8 +129,9 @@ function handleSuccessfulDataFetch(data) {
   if (data.length > 0) {
     console.log("We have " + data.length + " results!");
     updateResultsDisplay(data);
-    $app.components.cards.store.listings = data;
     console.log("Data fetched successfully");
+    $app.components.cards.store.listings = data;
+
     activateList(data);
 
     showResultsUI();
