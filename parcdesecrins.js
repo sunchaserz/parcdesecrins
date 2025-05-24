3ac3da422cdaa201a587f097940feb56eea97045
Bart Stassen <hello@bartstassen.com>
2025-05-24 17:15:17 +0200

    dsds

--- parcdesecrins.js
+++ parcdesecrins.js
@@ -1079,6 +1079,18 @@ function injectCSS() {
       width: 100%;
       margin: 0;
       box-sizing: border-box;
+      display: flex;
+      flex-direction: column;
+    }
+
+    #cards.grid-layout .uui-blogsection01_image-wrapper {
+      width: 100%;
+      margin-bottom: 1rem;
+    }
+
+    #cards.grid-layout .uui-blogsection01_content {
+      width: 100%;
+      padding: 0;
     }
 
     /* List layout - override Webflow grid */
